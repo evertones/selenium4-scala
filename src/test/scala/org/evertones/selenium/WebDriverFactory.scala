b868828f2f0e468c0265ea5e38318341ec537834
Everton Eckert Schneider <everton.schneider@gmail.com>
2022-10-15 00:12:18 -0300

    Updated WebDriverFactory.

--- src/test/scala/org/evertones/selenium/WebDriverFactory.scala
+++ src/test/scala/org/evertones/selenium/WebDriverFactory.scala
@@ -1,16 +1,14 @@
 package org.evertones.selenium
 
-import java.net.URL
-
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.firefox.FirefoxOptions
 import org.openqa.selenium.remote.RemoteWebDriver
+
+import java.net.URL
 
 class WebDriverFactory {
 
     def createRemoteInstance(): RemoteWebDriver = {
         val capabilities = new ChromeOptions().setAcceptInsecureCerts(true)
-        //val capabilities = new FirefoxOptions().setAcceptInsecureCerts(true)
 
         val remoteUrl    = new URL("http://localhost:4444")
         new RemoteWebDriver(remoteUrl, capabilities)
--- src/test/scala/org/evertones/test/Selenium4Spec.scala
+++ src/test/scala/org/evertones/test/Selenium4Spec.scala
@@ -1,9 +1,11 @@
 package org.evertones.test
 
 import org.evertones.selenium.WebDriverFactory
+import org.openqa.selenium.support.ui.Sleeper
 import org.openqa.selenium.{By, Keys, WebDriver}
 import org.scalatest.{FunSpec, GivenWhenThen, Matchers}
 
+import java.time.Duration
 import scala.util.Random
 
 
@@ -21,13 +23,19 @@ class Selenium4Spec
 
                 val input = webDriver.findElement(By.cssSelector("input[name='q']"))
 
+                try Sleeper.SYSTEM_SLEEPER.sleep(Duration.ofSeconds(9))
+                catch {
+                    case e: Exception =>
+                        System.out.println("There was an error waiting... " + e.getMessage)
+                }
+
                 input.sendKeys("test")
                 input.sendKeys(Keys.ENTER)
 
                 val testResultsStat = webDriver.findElement(By.id("result-stats"))
                 Given("Result: " + testResultsStat.toString)
 
-                testResultsStat.toString should not be null
+                testResultsStat.toString should not be empty
 
                 webDriver.quit()
             }
